24db6e913b81835ed7f8133af2686b07bc302742
Ondrej Sebl <ondrej.sebl@icloud.com>
2022-03-06 14:18:27 +0100

    schema

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
-const express = require('express')
-const app = express
-const cors = require('cors')
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require("dotenv").config('.env');
+
+const app = express();
+const port = process.env.PORT || 5500;
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Db connection success')
+})
+// app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
 
 app.listen(port, () => {
     // perform a database connection when server starts
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-
-    });
+    // dbo.connectToServer(function (err) {
+    //     if (err) console.error(err);
+    //
+    // });
     console.log(`Server is running on port: ${port}`);
 });
